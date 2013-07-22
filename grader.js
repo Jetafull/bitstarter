49f332840140a8835dac19289bd8574d5b41d296
jetafull <jetafull@gmail.com>
2013-07-23 03:19:29 +0900

    revise grader.js for hw3, tried callback

--- grader.js
+++ grader.js
@@ -27,6 +27,11 @@ var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
+// add for homework
+var rest = require('restler');
+var sys = require('util');
+var URL_DEFAULT = "http://thawing-eyrie-2235.herokuapp.com";
+
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -36,17 +41,24 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
+function download(url, callback) {
+  var resp = rest.get(url);
+  resp.on('complete', function(result) {
+    if(result instanceof Error) {
+      sys.puts('Error' + result.message);
+      this.retry(5000);
+      return;
+    }
+    callback(null, result);
+  });
+}
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
+var checkHtml = function(html, checks) {
+    $ = cheerio.load(html);
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
@@ -55,20 +67,47 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+// loads html from a file and checks it
+// for exports only
+function checkHtmlFile(filename, checks) {
+    return checkHtml(fs.readFileSync(filename), checks);
+}
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+    // create a asynch check
+function check(err, html) {
+    if(err) {
+      console.log("Error when getting html content" + err);
+      process.exit(1);
+    }
+    var checks = loadChecks(program.checks);
+    var checkJson = checkHtml(html, checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+    fs.writeFile("submission/part3.txt", outJson, function(err) {
+        if (err) throw err;
+        console.log('It\'s saved!');
+    });
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+        .option('-u, --url <url>', 'Address for website')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.file) {
+      fs.readFile(program.file, check);
+    } else if (program.url) {
+      download(program.url, check);
+    }
 } else {
+    exports.loadChecks = loadChecks;
     exports.checkHtmlFile = checkHtmlFile;
 }
