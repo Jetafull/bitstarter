9b4bfd6e1b1dd5aa35c152b423643010da5bbafb
Ubuntu <jetafull@gmail.com>
2013-07-23 17:13:17 +0000

    repair thermometer for index.html

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
   response.send(buf.toString("utf-8", 0, len-1));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
